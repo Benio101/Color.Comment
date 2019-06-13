7340ec11e7e0e1c0151259422070ee99f20cc3b2
Paweł Benetkiewicz <git@benio.me>
2019-06-13 22:45:40 +0200

    chore: bump version Signed-off-by: Paweł Benetkiewicz <git@benio.me>

--- Color.Comment/Package.cs
+++ Color.Comment/Package.cs
@@ -12,7 +12,7 @@ namespace Color.Comment
 		PackageAutoLoadFlags.BackgroundLoad
 	)]
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[InstalledProductRegistration("#110", "#112", "0.0.1", IconResourceID = 400)]
+	[InstalledProductRegistration("#110", "#112", "0.0.2", IconResourceID = 400)]
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[Guid("00000043-6F6C-6F72-2E43-6F6D6D2E6374")]
 	[ProvideOptionPage(typeof(OptionsPage), "Color.Comment", "Options", 0, 0, true)]
