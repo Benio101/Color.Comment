1e33c41eb605d05d1f8c5f1105ef872dd238a994
Paweł Benetkiewicz <git@benio.me>
2019-08-30 02:41:18 +0200

    refactor: misc Signed-off-by: Paweł Benetkiewicz <git@benio.me>

--- Color.Comment/Package.cs
+++ Color.Comment/Package.cs
@@ -1,32 +1,46 @@
-﻿using System;
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
 
 namespace Color.Comment
 {
-	[ProvideAutoLoad(
-		VSConstants.UICONTEXT.ShellInitialized_string,
-		PackageAutoLoadFlags.BackgroundLoad
-	)]
-	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[InstalledProductRegistration("#110", "#112", "0.0.1", IconResourceID = 400)]
-	[ProvideMenuResource("Menus.ctmenu", 1)]
-	[Guid("00000043-6F6C-6F72-2E43-6F6D6D2E6374")]
-	[ProvideOptionPage(typeof(OptionsPage), "Color.Comment", "Options", 0, 0, true)]
+	[
+		PackageRegistration
+		(
+			AllowsBackgroundLoading = true,
+			UseManagedResourcesOnly = true
+		),
+
+		ProvideAutoLoad
+		(
+			VSConstants.UICONTEXT.ShellInitialized_string,
+			PackageAutoLoadFlags.BackgroundLoad
+		),
+
+		InstalledProductRegistration("#110", "#112", "0.0.2", IconResourceID = 400),
+		Guid("00000043-6F6C-6F72-2E43-6F6D6D2E6374"),
+
+		ProvideMenuResource("Menus.ctmenu", 1),
+		ProvideOptionPage(typeof(OptionsPage), "Color.Comment", "Options", 0, 0, true)
+	]
+
 	public sealed class Package
-		: AsyncPackage
+	:
+		AsyncPackage
 	{
-		protected override async System.Threading.Tasks.Task InitializeAsync(
-			CancellationToken cancellationToken,
-			IProgress<ServiceProgressData> progress
-		){
+		protected override async Task InitializeAsync
+		(
+			CancellationToken              Token,
+			IProgress<ServiceProgressData> Progress
+		)
+		{
 			Project.Package = this;
 
-			await base.InitializeAsync(cancellationToken, progress);
-			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+			await base.InitializeAsync(Token, Progress);
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(Token);
 		}
 	}
 
